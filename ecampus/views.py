b3b2e74e5e8dc69ebc04dc285d4974869236d1ef
hyunmin <choihm9903@naver.com>
2022-04-08 00:30:40 +0900

    fix: path

--- ecampus/views.py
+++ ecampus/views.py
@@ -10,7 +10,7 @@ def login(id, password):
     options.add_argument("headless")
 
     # driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver = webdriver.Chrome('/home/sangmyung/chromedriver', options=options)
+    driver = webdriver.Chrome('/home/ubuntu/chromedriver', options=options)
     driver.get(URL)
     driver.find_element(By.ID, 'input-username').send_keys(id)
     driver.find_element(By.ID, 'input-password').send_keys(password)
