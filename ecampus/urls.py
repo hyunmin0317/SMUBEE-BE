4adc46508269fc8335c905e55ac3237028261d1f
hyunmin <choihm9903@naver.com>
2022-04-08 13:26:05 +0900

    feat: courses

--- ecampus/urls.py
+++ ecampus/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from ecampus import views
 
+app_name = 'ecampus'
+
 urlpatterns = [
     path('', views.home),
+    path('course/<str:code>/', views.detail, name='detail')
 ]
--- ecampus/views.py
+++ ecampus/views.py
@@ -4,30 +4,30 @@ from selenium.webdriver.common.by import By
 from django.shortcuts import render
 from webdriver_manager.chrome import ChromeDriverManager
 
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+# driver = webdriver.Chrome('/home/ubuntu/chromedriver', options=options)
 
 def login(id, password):
     URL = "https://ecampus.smu.ac.kr/login.php"
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    # driver = webdriver.Chrome('/home/ubuntu/chromedriver', options=options)
     driver.get(URL)
     driver.find_element(By.ID, 'input-username').send_keys(id)
     driver.find_element(By.ID, 'input-password').send_keys(password)
     driver.find_element(By.NAME, 'loginbutton').click()
     return driver.page_source
 
+
+
 def course(id, password):
     data = []
     url = login(id, password)
     soup = BeautifulSoup(url, 'html.parser')
-    courses = soup.find_all("div", class_="course-title")
+    courses = soup.find_all("div", class_="course_box")
     for course in courses:
-        dic = {'name':course.find("h3").text, 'prof':course.find("p").text}
+        dic = {'name':course.find("h3").text, 'prof':course.find("p").text, 'code':course.find("a")["href"].split("=")[1]}
         data.append(dic)
     return data
-
 
 def home(request):
     if request.method == 'GET':
@@ -43,3 +43,7 @@ def home(request):
         else:
             context = {'courses': courses}
             return render(request, 'home.html', context)
+
+def detail(request, code):
+    context = {'code':code}
+    return render(request, 'detail.html', context)
