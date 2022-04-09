a58f1c4907251fb7ebb4b21e48bab95857a353f6
hyunmin <choihm9903@naver.com>
2022-04-10 01:22:20 +0900

    feat: course-crawling

--- crawling/course2.py
+++ crawling/course2.py
@@ -12,10 +12,12 @@ def login(id, password):
     driver.find_element(By.ID, 'input-password').send_keys(password)
     driver.find_element(By.NAME, 'loginbutton').click()
 
+
 def logout():
     url = "https://ecampus.smu.ac.kr/login/logout.php?sesskey=743L2Q8XQd"
     driver.get(url)
     driver.find_element_by_xpath('//*[@id="notice"]/div/div[1]/form/div/input[1]').click()
+
 
 def course():
     url = "https://ecampus.smu.ac.kr"
@@ -26,22 +28,26 @@ def course():
     soup = BeautifulSoup(page, 'html.parser')
     courses = soup.find_all("div", class_="course_box")
     for course in courses:
-        dic = {'name':course.find("h3").text, 'prof':course.find("p").text, 'code':course.find("a")["href"].split("=")[1]}
+        dic = {'name': course.find("h3").text, 'prof': course.find("p").text,
+               'code': course.find("a")["href"].split("=")[1]}
         data.append(dic)
     return data
 
+
 def course_data(code):
-    url = "https://ecampus.smu.ac.kr/report/ubcompletion/user_progress.php?id="+code
+    url = "https://ecampus.smu.ac.kr/report/ubcompletion/user_progress.php?id=" + code
     driver.get(url)
     page = driver.page_source
+    data = []
+
     soup = BeautifulSoup(page, 'html.parser')
     body = soup.find("table", class_="user_progress")
-    courses = body.find_all("td", class_="text-left")
 
-    for course in courses:
-        print(course)
-        print(type(course))
-        print(course.find("td"))
+    if body is not None:
+        courses = body.find_all("td", class_="text-left")
+        for course in courses:
+            data.append(course.text)
+    return data
 
 
 if __name__ == '__main__':
@@ -52,11 +58,10 @@ if __name__ == '__main__':
 
     login("201911019", "1q2w3e4r!!")
     courses = course()
-    course_data("68630")
-    logout()
 
     if len(courses)==0:
         print("로그인 실패")
     else:
         for course in courses:
             print(course['name'], course['prof'], course['code'])
+            print(course_data(course['code']))
