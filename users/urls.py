9cbc207af27328fa5610b77e500d449a16accdb1
hyunmin <choihm9903@naver.com>
2022-05-17 01:17:53 +0900

    feat: ecampus-login

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 # api/urls.py
 from django.urls import path
-from .views import RegistrationAPI, LoginAPI, UserAPI
+from .views import LoginAPI, UserAPI
 
 urlpatterns = [
-    path("signup/", RegistrationAPI.as_view()),
     path("login/", LoginAPI.as_view()),
     path("users/", UserAPI.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,48 +1,35 @@
+from django.contrib.auth.models import User
 from rest_framework import permissions, generics, status
 from rest_framework.response import Response
 from knox.models import AuthToken
 from crawling import login
 from .models import Profile
-from .serializers import CreateUserSerializer, UserSerializer, LoginUserSerializer
+from .serializers import UserSerializer
 
 
-# 회원가입
-class RegistrationAPI(generics.GenericAPIView):
-    serializer_class = CreateUserSerializer
-
+# 이캠퍼스 로그인
+class LoginAPI(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         id, password = request.data["username"], request.data["password"]
         if login(id, password) == -1:
             body = {"message": "ecampus login error"}
             return Response(body, status=status.HTTP_400_BAD_REQUEST)
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        profile = Profile(user=user, password=password)
-        profile.save()
+
+        updated_rows = User.objects.filter(username=id)
+        if not updated_rows:
+            User.objects.create(username=id)
+        user = User.objects.get(username=id)
+
+        updated_rows = Profile.objects.filter(user=user).update(password=password)
+        if not updated_rows:
+            Profile.objects.create(user=user, password=password)
+
         _, token = AuthToken.objects.create(user)
         return Response(
             {
-                "username": user.username,
                 "token": token
             }
         )
-
-
-# 로그인
-class LoginAPI(generics.GenericAPIView):
-    serializer_class = LoginUserSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-        return Response(
-            {
-                "token": AuthToken.objects.create(user)[1],
-            }
-        )
-
 
 # 접속 유지중인지 확인
 class UserAPI(generics.RetrieveAPIView):
