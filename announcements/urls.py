e2f8e58ebc161ab7d7212e2650e3949889a89d6d
hyunmin <choihm9903@naver.com>
2022-07-04 22:14:20 +0900

    feat: filter

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AnnounceListAPI, update
+from .views import AnnounceListAPI, AnnounceFilterAPI, update
 
 urlpatterns = [
     path('all/', AnnounceListAPI.as_view()),
+    path('<str:campus>/', AnnounceFilterAPI.as_view()),
     path('update/', update),
 ]
--- announcements/views.py
+++ announcements/views.py
@@ -1,4 +1,5 @@
 import json
+from django.db.models import Q
 from django.http import HttpResponse
 from rest_framework.generics import ListAPIView
 from announcements.models import Announcement
@@ -9,6 +10,15 @@ from crawling import announce_update
 class AnnounceListAPI(ListAPIView):
     queryset = Announcement.objects.all().order_by('-number')
     serializer_class = AnnounceSerializer
+
+
+class AnnounceFilterAPI(ListAPIView):
+    serializer_class = AnnounceSerializer
+
+    def get_queryset(self):
+        campus = self.kwargs['campus']
+        queryset = Announcement.objects.filter(Q(campus='both')|Q(campus=campus)).order_by('-number')
+        return queryset
 
 
 def update(request):
