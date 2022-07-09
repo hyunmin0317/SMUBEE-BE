8bf209385b727f7b3a24d1cc3eed84a519a8492d
hyunmin <choihm9903@naver.com>
2022-07-09 21:44:00 +0900

    fix: announce-update

--- announcements/urls.py
+++ announcements/urls.py
@@ -3,6 +3,6 @@ from .views import AnnounceListAPI, AnnounceFilterAPI, update
 
 urlpatterns = [
     path('all/', AnnounceListAPI.as_view()),
-    path('<str:campus>/', AnnounceFilterAPI.as_view()),
     path('update/', update),
+    path('<str:campus>/', AnnounceFilterAPI.as_view()),
 ]
