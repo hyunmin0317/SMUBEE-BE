1d131e52cdefe6e2a3a9837610d369afce70c4f8
hyunmin <choihm9903@naver.com>
2022-08-20 12:59:06 +0900

    fix: Planner API

--- planners/models.py
+++ planners/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Plan(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.CharField(max_length=30)
+    course = models.CharField(max_length=30, null=True)
     title = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
     content = models.TextField()
     date = models.DateTimeField()
-    status = models.CharField(max_length=30)
-    checked = models.BooleanField(default=False)
+    status = models.CharField(max_length=30, null=True)
+    checked = models.BooleanField(default=False, null=True)
 
     def __str__(self):
         return self.title
