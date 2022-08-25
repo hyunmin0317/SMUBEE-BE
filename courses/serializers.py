1177d908ed811ffe6c6ee26e4bf88a01abba5c19
hyunmin <choihm9903@naver.com>
2022-08-25 19:29:34 +0900

    feat: Subject status

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
-from .models import Subject
+
+from planners.models import Plan
+from courses.models import Subject
 
 class SubjectSerializer(serializers.ModelSerializer):
+    course_status = serializers.SerializerMethodField()
+    assign_status = serializers.SerializerMethodField()
+
     class Meta:
         model = Subject
         fields = '__all__'
+
+    def get_course_status(self, obj):
+        all = Plan.objects.filter(code=obj.code, category='course').count()
+        done = Plan.objects.filter(code=obj.code, category='course', checked=True).count()
+
+        if all == 0:
+            return -1
+        return round(done / all * 100)
+
+    def get_assign_status(self, obj):
+        all = Plan.objects.filter(code=obj.code, category='assign').count()
+        done = Plan.objects.filter(code=obj.code, category='assign', checked=True).count()
+
+        if all == 0:
+            return -1
+        return round(done / all * 100)
