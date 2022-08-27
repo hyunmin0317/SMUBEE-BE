6e87181c5fed576b1efe7c7f8e086827fb4cdc78
hyunmin <choihm9903@naver.com>
2022-08-27 16:14:53 +0900

    feat: check data

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from courses.views import CourseAPI, AssignAPI, SubjectListAPI
+from courses.views import CourseAPI, AssignAPI, SubjectListAPI, check
 
 urlpatterns = [
     path('all/', SubjectListAPI.as_view()),
+    path('check/', check),
     path('<str:code>/course/', CourseAPI.as_view()),
     path('<str:code>/assign/', AssignAPI.as_view()),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,8 @@
+from django.db.models import Q
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
-
+from rest_framework.response import Response
 from courses.models import Subject
 from courses.serializers import SubjectSerializer
 from planners.models import Plan
@@ -27,3 +30,11 @@ class SubjectListAPI(ListAPIView):
 
     def get_queryset(self):
         return Subject.objects.filter(user=self.request.user)
+
+
+@api_view(['GET'])
+def check(request):
+    complete = Plan.objects.filter(Q(user=request.user) & (Q(category="course") | Q(category="assign")) & Q(checked=True)).count()
+    incomplete = Plan.objects.filter(Q(user=request.user) & (Q(category="course") | Q(category="assign")) & Q(checked=False)).count()
+    res = {'complete': complete, 'incomplete': incomplete}
+    return Response(res, status=status.HTTP_200_OK)
